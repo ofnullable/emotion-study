3b45132d2ffbc8dbc72fc60f883b915fa18fc981
ofnullable <ofnullable@gmail.com>
2020-01-01 20:49:50 +0900

    :sparkles: Add AvatarGroup component

---
+++ components/common/Avatar/AvatarGroup.js
@@ -0,0 +1,29 @@
+import React from 'react';
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+import { colors } from '../../../style/colors';
+
+const AvatarGroup = props => {
+  const children = React.Children.toArray(props.children);
+  return (
+    <div css={[avatarGroupStyle]}>
+      {children.map((child, index) => {
+        return React.cloneElement(child, {
+          style: { zIndex: children.length - index, ...child.props.style },
+        });
+      })}
+    </div>
+  );
+};
+
+const avatarGroupStyle = css`
+  display: flex;
+  & > div {
+    border: 2px solid ${colors.gray[0]};
+    &:not(:first-of-type) {
+      margin-left: -20px;
+    }
+  }
+`;
+
+export default AvatarGroup;
